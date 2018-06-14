0403b21687a3d554abc8966ac2d90350e46285d3
Raytine <1729051657@qq.com>
2018-06-14 13:44:26 +0800

    啊大苏打撒

---
+++ live2d/js/message.js
@@ -0,0 +1,217 @@
+function renderTip(template, context) {
+    var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
+    return template.replace(tokenReg, function (word, slash1, token, slash2) {
+        if (slash1 || slash2) {
+            return word.replace('\\', '');
+        }
+        var variables = token.replace(/\s/g, '').split('.');
+        var currentObject = context;
+        var i, length, variable;
+        for (i = 0, length = variables.length; i < length; ++i) {
+            variable = variables[i];
+            currentObject = currentObject[variable];
+            if (currentObject === undefined || currentObject === null) return '';
+        }
+        return currentObject;
+    });
+}
+//鼠标在某些元素上方时
+jQuery(document).ready(function ($) {
+    $('h2 a').click(function () {//标题被点击时
+        showMessage('正在用吃奶的劲加载《<span style="color:#0099cc;">' + $(this).text() + '</span>》请稍候');
+    });
+    $('h2 a').mouseover(function () {
+        showMessage('要看看《<span style="color:#0099cc;">' + $(this).text() + '</span>》这篇文章么？');
+    });
+    $('#prev-page').mouseover(function(){
+        showMessage('要翻到上一页吗?');
+    });
+    $('#next-page').mouseover(function(){
+        showMessage('要翻到下一页吗?');
+    });
+    $('#index-links li a').mouseover(function () {
+        showMessage('去 <span style="color:#0099cc;">' + $(this).text() + '</span> 逛逛');
+    });
+    $('.comments').mouseover(function () {
+        showMessage('<span style="color:#0099cc;">' + visitor + '</span> 向评论栏出发吧！');
+    });
+    $('#submit').mouseover(function () {
+        showMessage('确认提交了么？');
+    });
+    $('#s').focus(function () {
+        showMessage('输入你想搜索的关键词再按Enter(回车)键就可以搜索啦!');
+    });
+    $('#go-prev').mouseover(function () {
+        showMessage('点它可以后退哦！');
+    });
+    $('#go-next').mouseover(function () {
+        showMessage('点它可以前进哦！');
+    });
+    $('#refresh').mouseover(function () {
+        showMessage('点它可以重新载入此页哦！');
+    });
+    $('#go-home').mouseover(function () {
+        showMessage('点它就可以回到首页啦！');
+    });
+    $('#addfav').mouseover(function () {
+        showMessage('点它可以把此页加入书签哦！');
+    });
+    $('#nav-two a').mouseover(function () {
+        showMessage('嘘，从这里可以进入控制面板的哦！');
+    });
+    $('.post-category a').mouseover(function () {
+        showMessage('点击查看此分类下得所有文章');
+    });
+    $('.post-heat a').mouseover(function () {
+        showMessage('点它可以直接跳到评论列表处.');
+    });
+    $('#tho-shareto span a').mouseover(function () {
+        showMessage('你知道吗?点它可以分享本文到'+$(this).attr('title'));
+    });
+    $('#switch-to-wap').mouseover(function(){
+        showMessage('点击可以切换到手机版博客版面');
+    });
+});
+
+String.prototype.renderTip = function (context) {
+    return renderTip(this, context);
+};
+
+var re = /x/;
+console.log(re);
+re.toString = function() {
+    showMessage('哈哈，你打开了控制台，是想要看看我的秘密吗？', 5000);
+    return '';
+};
+
+$(document).on('copy', function (){
+    showMessage('你都复制了些什么呀，转载要记得加上出处哦~~', 5000);
+});
+
+function initTips(){
+    $.ajax({
+        cache: true,
+        url: `${message_Path}message.json`,
+        dataType: "json",
+        success: function (result){
+            $.each(result.mouseover, function (index, tips){
+                $(tips.selector).mouseover(function (){
+                    var text = tips.text;
+                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                    text = text.renderTip({text: $(this).text()});
+                    showMessage(text, 3000);
+                });
+            });
+            $.each(result.click, function (index, tips){
+                $(tips.selector).click(function (){
+                    var text = tips.text;
+                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                    text = text.renderTip({text: $(this).text()});
+                    showMessage(text, 3000);
+                });
+            });
+        }
+    });
+}
+initTips();
+
+(function (){
+    var text;
+    if(document.referrer !== ''){
+        var referrer = document.createElement('a');
+        referrer.href = document.referrer;
+        text = '嗨！来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友！';
+        var domain = referrer.hostname.split('.')[1];
+        if (domain == 'baidu') {
+            text = '嗨！ 来自 百度搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+        }else if (domain == 'so') {
+            text = '嗨！ 来自 360搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+        }else if (domain == 'google') {
+            text = '嗨！ 来自 谷歌搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+        }
+    }else {
+        if (window.location.href == `${home_Path}`) { //主页URL判断，需要斜杠结尾
+            var now = (new Date()).getHours();
+            if (now > 23 || now <= 5) {
+                text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？';
+            } else if (now > 5 && now <= 7) {
+                text = '早上好！一日之计在于晨，美好的一天就要开始了！';
+            } else if (now > 7 && now <= 11) {
+                text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
+            } else if (now > 11 && now <= 14) {
+                text = '中午了，工作了一个上午，现在是午餐时间！';
+            } else if (now > 14 && now <= 17) {
+                text = '午后很容易犯困呢，今天的运动目标完成了吗？';
+            } else if (now > 17 && now <= 19) {
+                text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~~';
+            } else if (now > 19 && now <= 21) {
+                text = '晚上好，今天过得怎么样？';
+            } else if (now > 21 && now <= 23) {
+                text = '已经这么晚了呀，早点休息吧，晚安~~';
+            } else {
+                text = '嗨~ 快来逗我玩吧！';
+            }
+        }else {
+            text = '欢迎阅读<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
+        }
+    }
+    showMessage(text, 12000);
+})();
+
+window.setInterval(showHitokoto,30000);
+
+function showHitokoto(){
+    $.getJSON('https://sslapi.hitokoto.cn/',function(result){
+        showMessage(result.hitokoto, 5000);
+    });
+}
+
+function showMessage(text, timeout){
+    if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
+    //console.log('showMessage', text);
+    $('.message').stop();
+    $('.message').html(text).fadeTo(200, 1);
+    if (timeout === null) timeout = 5000;
+    hideMessage(timeout);
+}
+
+function hideMessage(timeout){
+    $('.message').stop().css('opacity',1);
+    if (timeout === null) timeout = 5000;
+    $('.message').delay(timeout).fadeTo(200, 0);
+}
+
+function initLive2d (){
+    $('.hide-button').fadeOut(0).on('click', () => {
+        $('#landlord').css('display', 'none')
+    })
+    $('#landlord').hover(() => {
+        $('.hide-button').fadeIn(600)
+    }, () => {
+        $('.hide-button').fadeOut(600)
+    })
+}
+initLive2d ();
+//天气api
+var weather=Array();
+weather.state=false;
+$(document).ready(function(){
+    $.ajax({
+        dataType:"jsonp",
+        success:function(data){
+            if(data.success!=1){return;}
+            weather.state=true;
+            weather.c=Array();
+            weather.c[0]="今天是"+data.result[0].days+"，"+data.result[0].week;
+            weather.c[1]=data.result[0].citynm+"今天气温是"+data.result[0].temp_high+"°C到"+data.result[0].temp_low+"°C";
+            weather.c[2]=data.result[0].citynm+"今天天气是"+data.result[0].weather+"  "+data.result[0].temp_high+"°C~"+data.result[0].temp_low+"°C";
+            weather.c[3]=data.result[0].citynm+"今天风力是"+data.result[0].winp+"，"+data.result[0].wind;
+            weather.c[4]=data.result[1].citynm+"明天气温是"+data.result[1].temp_high+"°C到"+data.result[1].temp_low+"°C";
+            weather.c[5]=data.result[1].citynm+"明天天气是"+data.result[1].weather+"  "+data.result[1].temp_high+"°C~"+data.result[1].temp_low+"°C";
+            weather.c[6]=data.result[2].citynm+"后天气温是"+data.result[2].temp_high+"°C到"+data.result[2].temp_low+"°C";
+            weather.c[7]=data.result[2].citynm+"后天天气是"+data.result[2].weather+"  "+data.result[2].temp_high+"°C~"+data.result[2].temp_low+"°C";
+        },
+        type:"GET",
+        url:"http://api.myhloli.com/weather/?callback=?"
+    });
+});
